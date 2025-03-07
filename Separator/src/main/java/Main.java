4a32c93a5d86d3f1661f0cee488d2262b3f440b9
Martin Vartazaryan <131700005+3uPaeJLb@users.noreply.github.com>
2025-03-07 15:29:55 +0700

    Update Main.java

--- Separator/src/main/java/Main.java
+++ Separator/src/main/java/Main.java
@@ -3,15 +3,18 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-       
+        int length = args.length;
+        if (args.length < 2) {
+            System.out.println("usage: java Main <input_file> <output_file>");
+            return;
+        }
         String inputFile = args[0];
         String outputFile = args[1];
-        
         DataParser dataParser = new DataParser();
         DataWriter dataWriter = new DataWriter();
-        
         dataParser.readFromFile(inputFile);
         dataWriter.fillCSV(dataParser.wordsMap, outputFile);
     }
 }
+
 
